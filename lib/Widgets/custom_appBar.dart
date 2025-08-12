cd408b67554a97fd2b61e8ffa79ecb28158aad3d
‪Hazem ahmed mohamed sayed‬‏ <155602414+Hazem2911@users.noreply.github.com>
2025-08-12 19:02:12 +0300

    Made a custom appBar

--- lib/Widgets/custom_appBar.dart
+++ lib/Widgets/custom_appBar.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/Widgets/Clickable_Icon.dart';
 
-PreferredSizeWidget customAppBar({required String title, List<Widget>? actions}) {
+PreferredSizeWidget customAppBar({
+  required String title,
+  List<Widget>? actions,
+}) {
   return AppBar(
+    automaticallyImplyLeading: false,
     backgroundColor: const Color(0xff303030),
     title: Text(
       title,
       style: const TextStyle(color: Colors.white, fontSize: 30),
     ),
-    actions: actions ??
+    titleSpacing: 15,
+
+    actions:
+        actions ??
         [
           const Padding(
-            padding: EdgeInsets.only(right: 15.0),
+            padding: EdgeInsets.only(right: 8.0),
             child: Clickableicon(icon: Icons.search),
           ),
         ],
